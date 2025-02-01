c4e368b26a6f8626ad58e1352c9d979bc9ce6697
XxDrancerxX <idmdiazmora98@gmail.com>
2025-02-01 01:50:08 +0000

    return

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird", "The Cat"];
+let action = ["ate", "peed", "crushed", "broke", "Took"];
+let what = ["my homework", "my phone", "the car", "My Adapter"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function excuse() {
+  let whoRange = Math.floor(Math.random() * who.length);
+  let actionRange = Math.floor(Math.random() * action.length);
+  let whatRange = Math.floor(Math.random() * what.length);
+  let whenRange = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let excuseDisplay = who[whoRange];
+}
+
+Excuse();
